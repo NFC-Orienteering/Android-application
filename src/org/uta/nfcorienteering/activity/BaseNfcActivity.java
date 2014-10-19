dc60434bc0a89a5dfe16138168dcfcacc3b72d58
Kelvin Yang <yzdedream@gmail.com>
2014-10-19 15:55:28 +0300

    fix problem with running on non-NFC device

---
+++ src/org/uta/nfcorienteering/activity/BaseNfcActivity.java
@@ -0,0 +1,211 @@
+package org.uta.nfcorienteering.activity;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+import org.uta.nfcorienteering.utility.AppConfig;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentFilter.MalformedMimeTypeException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
+
+public abstract class BaseNfcActivity extends Activity{
+
+	NfcAdapter nfcAdapter;
+	Tag detectedTag;
+	public static final String MIME_TEXT_PLAIN = "text/plain";
+    public static final String TAG = "NfcDemo";
+    String tagId;
+	
+    abstract public void postNfcRead(String result);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+		
+		if(nfcAdapter == null){
+			Toast.makeText(this, "Your device does not support NFC.", Toast.LENGTH_LONG).show();
+		}
+		else {
+			if(!nfcAdapter.isEnabled()){
+				Toast.makeText(this, "Your NFC-reader is disabled. Enable NFC-reader to continue", Toast.LENGTH_LONG).show();
+				startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
+			}
+		}
+		
+	}
+	protected void onResume() {
+        super.onResume();
+         
+        /**
+         * It's important, that the activity is in the foreground (resumed). Otherwise
+         * an IllegalStateException is thrown.
+         */
+        if (AppConfig.isRealDevice) {
+        	setupForegroundDispatch(this, nfcAdapter);
+		}
+    }
+     
+	 public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+    	
+    	 final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+         intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+  
+         final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+  
+         IntentFilter[] filters = new IntentFilter[1];
+         String[][] techList = new String[][]{};
+  
+         // Notice that this is the same filter as in our manifest.
+         filters[0] = new IntentFilter();
+         filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+         filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+         try {
+             filters[0].addDataType(MIME_TEXT_PLAIN);
+         } catch (MalformedMimeTypeException e) {
+             throw new RuntimeException("Check your mime type.");
+         }
+          
+         adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+		
+	}
+	@Override
+    protected void onPause() {
+        /**
+         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
+         */
+		if (AppConfig.isRealDevice) {
+			stopForegroundDispatch(this, nfcAdapter);
+		}
+         
+        super.onPause();
+    }
+     
+	public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
+    }
+	
+	@Override
+    protected void onNewIntent(Intent intent) {
+        /**
+         * This method gets called, when a new Intent gets associated with the current activity instance.
+         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
+         * at the documentation.
+         *
+         * In our case this method gets called, when the user attaches a Tag to the device.
+         */
+        readTagId(intent);
+    }
+	
+	private void readTagId(Intent intent) {
+	    String action = intent.getAction();
+	    if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+	         
+	        String type = intent.getType();
+	        if (MIME_TEXT_PLAIN.equals(type)) {
+	 
+	            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+	            new NdefReaderTask().execute(tag);
+	             
+	        } else {
+	            Log.d(TAG, "Wrong mime type: " + type);
+	        }
+	    } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+	         
+	        // In case we would still use the Tech Discovered Intent
+	        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+	        String[] techList = tag.getTechList();
+	        String searchedTech = Ndef.class.getName();
+	         
+	        for (String tech : techList) {
+	            if (searchedTech.equals(tech)) {
+	                new NdefReaderTask().execute(tag);
+	                break;
+	            }
+	        }
+	    }
+	}
+
+
+	class NdefReaderTask extends AsyncTask<Tag, Void, String> {
+		
+	    private static final String TAG = "NFC-demo";
+	    
+
+		protected String doInBackground(Tag... params) {
+	        Tag tag = params[0];
+	         
+	        Ndef ndef = Ndef.get(tag);
+	        if (ndef == null) {
+	            // NDEF is not supported by this Tag.
+	            return null;
+	        }
+	 
+	        NdefMessage ndefMessage = ndef.getCachedNdefMessage();
+	 
+	        NdefRecord[] records = ndefMessage.getRecords();
+	        for (NdefRecord ndefRecord : records) {
+	            if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+	                try {
+	                    return readText(ndefRecord);
+	                } catch (UnsupportedEncodingException e) {
+	                    Log.e(TAG, "Unsupported Encoding", e);
+	                }
+	            }
+	        }
+	 
+	        return null;
+	    }
+	     
+	    private String readText(NdefRecord record) throws UnsupportedEncodingException {
+	        /*
+	         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
+	         *
+	         * http://www.nfc-forum.org/specs/
+	         *
+	         * bit_7 defines encoding
+	         * bit_6 reserved for future use, must be 0
+	         * bit_5..0 length of IANA language code
+	         */
+	    	
+	        byte[] payload = record.getPayload();
+	 
+	        // Get the Text Encoding
+	        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+	 
+	        // Get the Language Code
+	        int languageCodeLength = payload[0] & 0063;
+	         
+	        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+	        // e.g. "en"
+	         
+	        // Get the Text
+	        return new String(payload,
+	        		languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+	    }
+	     
+	    @Override
+	    protected void onPostExecute(String result) {
+	        if (result != null) {
+	            postNfcRead(result);
+	        }
+	    }
+	}
+
+}
+
+
+
+
---
+++ src/org/uta/nfcorienteering/utility/AppConfig.java
@@ -0,0 +1,6 @@
+package org.uta.nfcorienteering.utility;
+
+public class AppConfig {
+	public static final boolean isRealDevice = false;
+
+}
