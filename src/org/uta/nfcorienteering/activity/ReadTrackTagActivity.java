53a64f041ba49e00c915fef69bd3d1f0b8643081
Nusrat057 <ns99709@student.uta.fi>
2014-10-19 19:10:12 +0300

    I just add the condition in the checkpoint.java where if it gets the right track id it ll return true value otherwise in case of null or wrong track id it will put an exception.

--- src/org/uta/nfcorienteering/activity/ReadTrackTagActivity.java
+++ src/org/uta/nfcorienteering/activity/ReadTrackTagActivity.java
@@ -1,28 +1,12 @@
 package org.uta.nfcorienteering.activity;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
-
 import org.uta.nfcorienteering.R;
 
-import android.app.Activity;
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.IntentFilter.MalformedMimeTypeException;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
-import android.nfc.NfcAdapter;
-import android.nfc.Tag;
-import android.nfc.tech.Ndef;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class ReadTrackTagActivity extends BaseNfcActivity {
