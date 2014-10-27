9202d45f53243cfa80fdee746193e46d11ace7cd
Kelvin Yang <yzdedream@gmail.com>
2014-10-27 16:36:36 +0200

    update on gesture image view

--- src/org/uta/nfcorienteering/activity/MapActivity.java
+++ src/org/uta/nfcorienteering/activity/MapActivity.java
@@ -3,8 +3,9 @@ package org.uta.nfcorienteering.activity;
 import org.uta.nfcorienteering.R;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.ViewGroup;
 import android.widget.LinearLayout.LayoutParams;
 
 import com.polites.android.GestureImageView;
@@ -16,9 +17,13 @@ public class MapActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		initGestureImageView();
+		// initGestureImageView();
 	}
 
+	/*
+	 * This is another way of setting GestureImageView from athor's tutorial
+	 */
+	@SuppressWarnings("unused")
 	private void initGestureImageView() {
 		LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT,
 				LayoutParams.WRAP_CONTENT);
@@ -27,7 +32,17 @@ public class MapActivity extends Activity {
 		view.setImageResource(R.drawable.ic_launcher);
 		view.setLayoutParams(params);
 
-		ViewGroup layout = (ViewGroup) findViewById(R.id.gesture_image_view);
-		layout.addView(view);
+		// ViewGroup layout = (ViewGroup) findViewById(R.id.layout);
+		// layout.addView(view);
+	}
+
+	private void setImageDrable(Drawable drawable) {
+		GestureImageView imageView = (GestureImageView) findViewById(R.id.gesture_image_view);
+		imageView.setImageDrawable(drawable);
+	}
+
+	private void setImageBitmap(Bitmap bitmap) {
+		GestureImageView imageView = (GestureImageView) findViewById(R.id.gesture_image_view);
+		imageView.setImageBitmap(bitmap);
 	}
 }
