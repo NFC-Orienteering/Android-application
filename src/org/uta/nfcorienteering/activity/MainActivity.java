4c65a9020daccf1db212dcfda1afd3c36ae8962e
Kelvin Yang <yzdedream@gmail.com>
2014-11-19 22:41:01 +0200

    merge string.xml

--- src/org/uta/nfcorienteering/activity/MainActivity.java
+++ src/org/uta/nfcorienteering/activity/MainActivity.java
@@ -3,6 +3,7 @@ package org.uta.nfcorienteering.activity;
 import org.uta.nfcorienteering.R;
 
 import android.app.Activity;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -29,6 +30,22 @@ public class MainActivity extends Activity {
 		Intent intent = new Intent(this, ReadTrackTagActivity.class);
 		startActivity(intent);
 		
+	}
+	
+	public void onClick(View v){
+		switch (v.getId()) {
+		case R.id.history_btn:
+			onHistoryButtonClick(v);
+			break;
+
+		default:
+			break;
+		}
+	}
+	
+	private void onHistoryButtonClick(View v){
+		Intent intent = new Intent(MainActivity.this,ResultHistoryActivity.class);
+		startActivity(intent);
 	}
 
 }
--- src/org/uta/nfcorienteering/event/OrienteeringRecord.java
+++ src/org/uta/nfcorienteering/event/OrienteeringRecord.java
@@ -4,21 +4,37 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class OrienteeringRecord implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8382206012435372165L;
+
 	private String nickname = "";
+	private String finishDate = "";
 	private ArrayList<Punch> punches = null;
-	
-	
+
 	public String getNickname() {
 		return nickname;
 	}
+
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
+
 	public ArrayList<Punch> getPunches() {
 		return punches;
 	}
+
 	public void setPunches(ArrayList<Punch> punches) {
 		this.punches = punches;
 	}
-	
+
+	public String getFinishDate() {
+		return finishDate;
+	}
+
+	public void setFinishDate(String finishDate) {
+		this.finishDate = finishDate;
+	}
+
 }
